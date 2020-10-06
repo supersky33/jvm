2a8e802310910656470447c66da73abece45a7d4
supersky33 <512122100@qq.com>
2020-10-06 22:44:02 +0800

    1

--- src/main/java/threadPoolTest.java
+++ src/main/java/threadPoolTest.java
@@ -1,19 +1,32 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class threadPoolTest {
 
     public static void main(String[] args) {
 
-        ExecutorService pool = Executors.newFixedThreadPool(5);
+        //ExecutorService pool = Executors.newFixedThreadPool(5);
 
         Executors.newSingleThreadExecutor();
 
         Executors.newCachedThreadPool();
 
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(
+                2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy());
+
         for (int i = 0; i < 10; i++) {
             pool.execute(()->{
                 System.out.println(Thread.currentThread().getName() + "-办理业务");
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             });
         }
 
